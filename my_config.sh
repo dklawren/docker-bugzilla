ee63d2b1707f301b55eea984532c0e5cb2495fb1
David Lawrence <dkl@mozilla.com>
2014-12-16 17:25:36 -0500

    More personal changes

--- my_config.sh
+++ my_config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Add any custom setup instructions here
 
+export $HOME=/home/$BUGZILLA_USER
+
 # Custom dependencies
 $CPANM IPC::System::Simple
 $CPANM App::Cmd
@@ -15,20 +17,25 @@ $CPANM JSON::RPC::Client
 
 # Bugzilla dev manager configuration
 git clone https://github.com/dklawren/bugzilla-dev-manager.git \
-    -b app /home/$BUGZILLA_USER/devel/bugzilla-dev-manager
-mkdir /home/$BUGZILLA_USER/devel/bin
-ln -sf /home/$BUGZILLA_USER/devel/bugzilla-dev-manager/bz \
-    /home/$BUGZILLA_USER/devel/bin/bz
-cp /home/$BUGZILLA_USER/devel/bugzilla-dev-manager/bz-dev.conf-sample \
+    -b app $HOME/devel/bugzilla-dev-manager
+mkdir $HOME/devel/bin
+ln -sf $HOME/devel/bugzilla-dev-manager/bz $HOME/devel/bin/bz
+cp $HOME/devel/bugzilla-dev-manager/bz-dev.conf-sample \
     /etc/bz-dev.conf
 
 # Home dotfiles
 export HOME=/home/$BUGZILLA_USER
-git clone https://github.com/dklawren/homedir \
-    /home/$BUGZILLA_USER/homedir
-cd /home/$BUGZILLA_USER/homedir
-./makedotfiles.sh
-ln -sf /home/$BUGZILLA_USER/devel/.bz-dev /home/$BUGZILLA_USER/.bz-dev
+git clone https://github.com/dklawren/homedir $HOME/homedir
+cd $HOME/homedir && ./makedotfiles.sh
+ln -sf $HOME/devel/.bz-dev $HOME/.bz-dev
 
-chmod 711 /home/$BUGZILLA_USER
-chown -R $BUGZILLA_USER.$BUGZILLA_USER /home/$BUGZILLA_USER
+# Vim configuration
+git clone https://github.com/dklawren/dotvim $HOME/.vim
+cd $HOME/.vim
+git submodule update --init
+ln -sf $HOME/.vim/rc/vimrc $HOME/.vimrc
+git clone https://github.com/powerline/fonts.git $HOME/powerline-fonts
+cd $HOME/powerline-fonts && ./install.sh
+
+# Permissions fixes
+chmod 711 $HOME && chown -R $BUGZILLA_USER.$BUGZILLA_USER $HOME
