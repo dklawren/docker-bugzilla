dad79970914a01bc8eb366aa23d39611c26a0817
David Lawrence <dkl@mozilla.com>
2015-05-15 09:23:33 -0400

    - Moved dependency install to separate script to it can also be ran from runtests.sh in case the branch is changed. - Added x11vnc for remove viewing of running selenium tests - Fixed issue with Firefox crashing in recent updates.

--- bugzilla_config.sh
+++ bugzilla_config.sh
@@ -2,34 +2,13 @@
 
 cd $BUGZILLA_HOME
 
-# Install Perl dependencies
-CPANM="cpanm --quiet --notest --skip-satisfied"
-
-if [ "$GITHUB_BASE_BRANCH" == "master" ]; then
-    perl checksetup.pl --cpanfile
-    $CPANM --installdeps --with-recommends --with-all-features \
-        --without-feature oracle --without-feature sqlite --without-feature pg .
-else
-    # Some modules are explicitly installed due to strange dependency issues
-    $CPANM Software::License
-    $CPANM HTML::FormatText::WithLinks
-    $CPANM DBD::mysql
-    $CPANM --installdeps --with-recommends .
-fi
-
-# For testing support
-$CPANM Test::WWW::Selenium
-$CPANM Pod::Coverage
-$CPANM Pod::Checker
-
-# Remove CPAN build files to minimize disk usage
-rm -rf /root/.cpanm
-
 # Configure database
 /usr/bin/mysqld_safe &
 sleep 5
 mysql -u root mysql -e "GRANT ALL PRIVILEGES ON *.* TO bugs@localhost IDENTIFIED BY 'bugs'; FLUSH PRIVILEGES;"
 mysql -u root mysql -e "CREATE DATABASE bugs CHARACTER SET = 'utf8';"
+
 perl checksetup.pl /checksetup_answers.txt
 perl checksetup.pl /checksetup_answers.txt
+
 mysqladmin -u root shutdown
